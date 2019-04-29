f254523619f81647522308b48dd54556ce28d791
Amplemind <amplemind@IsaacVazquez-Macbook.local>
2019-04-29 08:11:57 -0500

    Primer commit

---
+++ src/app/auth/dashboard/dashboard.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { EstadisticaComponent } from 'src/app/ingreso-egreso/estadistica/estadistica.component';
+import { IngresoEgresoComponent } from 'src/app/ingreso-egreso/ingreso-egreso.component';
+import { DetalleComponent } from 'src/app/ingreso-egreso/detalle/detalle.component';
+
+export const dashboardRoutes: Routes = [
+  { path: '', component: EstadisticaComponent },
+  { path: 'ingreso-egreso', component: IngresoEgresoComponent },
+  { path: 'detalle', component: DetalleComponent },
+];
