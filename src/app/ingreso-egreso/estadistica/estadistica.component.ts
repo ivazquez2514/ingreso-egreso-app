dce660aa3b8340478e49fe69ed3e15afa1a107ed
Amplemind <amplemind@IsaacVazquez-Macbook.local>
2019-05-02 15:48:45 -0500

    Hotfix seccion 10

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { IngresoEgreso } from '../ingreso-egreso.model';
 import { Label, MultiDataSet } from 'ng2-charts';
+import * as fromIngresoEgreso from '../ingreso-egreso.reducer';
 
 @Component({
   selector: 'app-estadistica',
